6081c2e8ab758d6d1bf67de641452647d65f7e81
Rogue-01 <113165538+Rogue-01@users.noreply.github.com>
2023-03-24 10:42:11 -0600

    Add project files.

---
+++ FinalProject/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Connect4
+{
+
+    //have four classes: Board, Player, Move, check win
+
+    //Board class
+    public class Board
+    {
+        private char[,] board = new char[6, 7];
+        public Board()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    board[i, j] = ' ';
+                }
+            }
+        }
+        public void PrintBoard()
+        {
+            Console.WriteLine("  1 2 3 4 5 6 7");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(board[i, j] + "|");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    //Player class
+    public class Player
+    {
+        private string name;
+        private char symbol;
+        public Player(string name, char symbol)
+        {
+            this.name = name;
+            this.symbol = symbol;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public char GetSymbol()
+        {
+            return symbol;
+        }
+    }
+    //Move class
+
+    //check win class
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //make board object
+            Board board = new Board();
+            board.PrintBoard();
+
+            //ask player for name
+            Console.WriteLine("Player 1, what is your name?");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Player 2, what is your name?");
+            string name2 = Console.ReadLine();
+
+            //ask player for symbol (X or O)
+            Console.WriteLine("Player 1, what symbol do you want to use? (X or O)");
+            char symbol1 = Console.ReadLine()[0];
+            Console.WriteLine("Player 2, what symbol do you want to use? (X or O)");
+            char symbol2 = Console.ReadLine()[0];
+
+            //create player objects
+            Player player1 = new Player(name1, symbol1);
+            Player player2 = new Player(name2, symbol2);
+            //create move objects
+            //create check win object
+            //loop until win
+
+        }
+    }
+}
