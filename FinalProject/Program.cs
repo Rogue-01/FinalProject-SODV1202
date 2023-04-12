f111c03de3521086829d2732d1d252f29917e68f
Rogue-01 <113165538+Rogue-01@users.noreply.github.com>
2023-04-12 15:28:35 -0600

    idk

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,7 +1,16 @@
-﻿using System;
+﻿using connectFour;
+using System;
 using System.Dynamic;
 using System.Reflection;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
 
+//Board - This class will represent the game board and hold the current state of the game. It should have methods to check for wins and add pieces to the board.
+//Player - This class will be an abstract base class for the two types of players in the game, human and computer players.It should have methods for getting and setting the player's name, and making moves on the board.
+//HumanPlayer - This class will implement the Player class and represent a human player. It should have a method for getting input from the user to make a move on the board.
+//ComputerPlayer - This class will implement the Player class and represent a computer player. It should have a method for generating a move on the board.
+//GameController - This class will handle the game flow, including creating the board, creating the players, and running the game loop. It should have methods for starting and ending the game, as well as handling player turns and displaying the board.
+//ConsoleView - This class will handle input and output from the console. It should have methods for displaying the game board and getting input from the user.
 namespace connectFour
 {
     //Board class
